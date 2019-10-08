81a85511e08d38fd71263f857aed825824701bc0
Tobi Situ <tobisitu@gmail.com>
2019-10-08 19:24:08 -0400

    Updates

---
+++ precache-manifest.15fa95b0dd7f2c1cacf44ad1002d0083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f514dad2807b8c08e2c93f9010165f",
+    "url": "/FriendsTvShow/index.html"
+  },
+  {
+    "revision": "6529e1bdffc39c4dcf5d",
+    "url": "/FriendsTvShow/static/css/main.b27e1912.chunk.css"
+  },
+  {
+    "revision": "97a49e0bcec72a879688",
+    "url": "/FriendsTvShow/static/js/2.6be8d630.chunk.js"
+  },
+  {
+    "revision": "6529e1bdffc39c4dcf5d",
+    "url": "/FriendsTvShow/static/js/main.e43c9858.chunk.js"
+  },
+  {
+    "revision": "1f220d03538a2c66f2ad",
+    "url": "/FriendsTvShow/static/js/runtime-main.57d934bd.js"
+  },
+  {
+    "revision": "03d08b5903a894201e3ebccde7bbf0df",
+    "url": "/FriendsTvShow/static/media/friends.03d08b59.png"
+  }
+]);
